13460f1682a0a9a0aa8543421a90fa72af1ddfbe
nicolamarino <nicolamarino527@gmail.com>
2024-12-17 18:52:35 +0100

    commit

--- js/scipt.js
+++ js/scipt.js
@@ -10,3 +10,30 @@
 
 
 
+// 1-chiediamo all utente di inserire le due variabili (età, numero di kilometri).
+let age = prompt("inserisci la tua età")
+let lenght = prompt("inserisci il numero di chilometri")
+
+
+// 2-ora stabiliamo il prezzo del prezzo in base alla lunghezza della tratta.
+let price = lenght * 0.21;
+
+// 3-iseriamo le variabili del prezzo in base all'età:
+
+//-se l'età è compresa tra i 19 e i 64 anni il prezzo è invariato:
+let discountPrice = price;
+
+//-se l'età é minore di 19 anni si attua uno sconto del 20%:
+if (age < 19) {
+    discountPrice = price - (price * 20 / 100);
+
+//-se invece l'età é maggiore di 19 anni si attua uno sconto del 40%:
+} else if (age>65) {
+    discountPrice = price - (price * 40 / 100);
+}
+
+// 4-convertiamo il prezzo ad un valore avente due decimali:
+let finalPrice = discountPrice.toFixed(2);
+
+// 5-stampiamo al cliente il valore in euro del csto delò biglietto:
+console.log(`Il prezzo del tuo biglietto è di: ${finalPrice} €.`);
